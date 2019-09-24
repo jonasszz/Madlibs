172d235b7fc6224ff01a23e3e95c45ee8e810911
Jonas Fernandez <fern0161@algonquinlive.com>
2019-09-23 23:39:37 -0400

    Completes the Madlibs assignment

--- script.js
+++ script.js
@@ -7,62 +7,60 @@ var littleRed = {
   verbs: ['ran', 'hear', 'see']
 }
 
-console.log('Little Red Riding Hood : Part 1');
+console.log('Little Red Riding Hood : Part 1')
 console.log('')
 console.log(`One day, Little ${littleRed.color} Riding Hood was going through the forest carrying a 
 basket of ${littleRed.pluralNouns[0]} for her grandmother. Suddenly, she met a big 
-${littleRed.adjective} wolf.`);
+${littleRed.adjective} wolf.`)
 console.log('')
-console.log(`"${littleRed.exclamation}" said the wolf. "Where are you going little ${littleRed.sillyWord}?"`);
+console.log(`"${littleRed.exclamation}" said the wolf. "Where are you going little ${littleRed.sillyWord}?"`)
 console.log('')
 console.log(`"I'm going to my grandmother's house," she said. Then the wolf 
-${littleRed.verbs[0]} away.`);
+${littleRed.verbs[0]} away.`)
 console.log('')
 console.log(`When Miss Riding Hood got to her grandmother's house, the wolf was in bed 
-dressed like her grandmother.`);
+dressed like her grandmother.`)
 console.log('')
-console.log(`"My, Grandmother," she said. "What big ${littleRed.pluralNouns[1]} you have."`);
+console.log(`"My, Grandmother," she said. "What big ${littleRed.pluralNouns[1]} you have."`)
 console.log('')
-console.log(`"The better to ${littleRed.verbs[1]} you with," said the wolf.`);
+console.log(`"The better to ${littleRed.verbs[1]} you with," said the wolf.`)
 console.log('')
-console.log(`"And, Grandmother," she said, "what big ${littleRed.pluralNouns[2]}" you have."`);
+console.log(`"And, Grandmother," she said, "what big ${littleRed.pluralNouns[2]}" you have."`)
 console.log('')
-console.log(`The wolf said, "The better to ${littleRed.verbs[2]} you with."`);
+console.log(`The wolf said, "The better to ${littleRed.verbs[2]} you with."`)
 console.log('')
 console.log(`And then she said, "What big ${littleRed.pluralNouns[3]}" you have, Grandmother." But 
-the wolf said nothing. He had just died of indigestion from eating Grandmother`);
-
-littleRed.color = 'Blue';
-littleRed.pluralNouns[2] = 'nose';
-littleRed.exclamation = 'Hey';
-littleRed.verbs[1] = 'smell';
+the wolf said nothing. He had just died of indigestion from eating Grandmother`)
+littleRed.color = 'Blue'
+littleRed.pluralNouns[2] = 'nose'
+littleRed.exclamation = 'Hey'
+littleRed.verbs[1] = 'smell'
 
 console.log('')
-console.log('Little Red Riding Hood : Part 2');
+console.log('Little Red Riding Hood : Part 2')
 console.log('')
 console.log(`One day, Little ${littleRed.color} Riding Hood was going through the forest carrying a 
 basket of ${littleRed.pluralNouns[0]} for her grandmother. Suddenly, she met a big 
-${littleRed.adjective} wolf.`);
+${littleRed.adjective} wolf.`)
 console.log('')
-console.log(`"${littleRed.exclamation}" said the wolf. "Where are you going little ${littleRed.sillyWord}?"`);
+console.log(`"${littleRed.exclamation}" said the wolf. "Where are you going little ${littleRed.sillyWord}?"`)
 console.log('')
 console.log(`"I'm going to my grandmother's house," she said. Then the wolf 
-${littleRed.verbs[0]} away.`);
+${littleRed.verbs[0]} away.`)
 console.log('')
 console.log(`When Miss Riding Hood got to her grandmother's house, the wolf was in bed 
-dressed like her grandmother.`);
+dressed like her grandmother.`)
 console.log('')
-console.log(`"My, Grandmother," she said. "What big ${littleRed.pluralNouns[1]} you have."`);
+console.log(`"My, Grandmother," she said. "What big ${littleRed.pluralNouns[1]} you have."`)
 console.log('')
-console.log(`"The better to ${littleRed.verbs[1]} you with," said the wolf.`);
+console.log(`"The better to ${littleRed.verbs[1]} you with," said the wolf.`)
 console.log('')
-console.log(`"And, Grandmother," she said, "what big ${littleRed.pluralNouns[2]}" you have."`);
+console.log(`"And, Grandmother," she said, "what big ${littleRed.pluralNouns[2]}" you have."`)
 console.log('')
-console.log(`The wolf said, "The better to ${littleRed.verbs[2]} you with."`);
+console.log(`The wolf said, "The better to ${littleRed.verbs[2]} you with."`)
 console.log('')
 console.log(`And then she said, "What big ${littleRed.pluralNouns[3]}" you have, Grandmother." But 
-the wolf said nothing. He had just died of indigestion from eating Grandmother`);
-
+the wolf said nothing. He had just died of indigestion from eating Grandmother`)
 
 var snowWhite = {
   pluralNouns: ['tales', 'dwarfs'],
@@ -71,25 +69,25 @@ var snowWhite = {
   nouns: ['castle', 'cottage', 'world', 'kiss', 'apple'],
   color: 'White',
   partOfBody: 'cheek',
-  adverb: 'happily',
+  adverb: 'happily'
 }
 
 console.log('')
 console.log('Snow White : Part 1')
 console.log('')
 console.log(`One of the most popular fairy ${snowWhite.pluralNouns[0]} of all time is Snow White and the 
-${snowWhite.number} ${snowWhite.pluralNouns[1]}.`);
+${snowWhite.number} ${snowWhite.pluralNouns[1]}.`)
 console.log('')
 console.log(`Snow White is a princess whose ${snowWhite.adjectives[0]} beauty threatens her stepmother, 
 the queen. Snow White is forced to flee from the ${snowWhite.nouns[0]} in which she lives 
-and hide in a nearby ${snowWhite.nouns[1]}.`);
+and hide in a nearby ${snowWhite.nouns[1]}.`)
 console.log('')
 console.log(`Once there, she is discovered by ${snowWhite.adjectives[1]} animals who guide her to the 
 ${snowWhite.adjectives[2]} cottage of the seven dwarfs. The dwarfs take care of her until 
 the prince, who has traveled the four corners of the ${snowWhite.nouns[2]} in search of 
 Snow ${snowWhite.color}, arrives and gives her a magical ${snowWhite.nouns[3]} on her 
 ${snowWhite.partOfBody}, which miraculously bring her back to life after she eats 
-a poisonous ${snowWhite.nouns[4]}. Snow White and the prince live ${snowWhite.adverb} ever after.`);
+a poisonous ${snowWhite.nouns[4]}. Snow White and the prince live ${snowWhite.adverb} ever after.`)
 console.log('')
 
 snowWhite.number = 5
@@ -102,17 +100,16 @@ console.log('')
 console.log('Snow White : Part 2')
 console.log('')
 console.log(`One of the most popular fairy ${snowWhite.pluralNouns[0]} of all time is Snow White and the 
-${snowWhite.number} ${snowWhite.pluralNouns[1]}.`);
+${snowWhite.number} ${snowWhite.pluralNouns[1]}.`)
 console.log('')
 console.log(`Snow White is a princess whose ${snowWhite.adjectives[0]} beauty threatens her stepmother, 
 the queen. Snow White is forced to flee from the ${snowWhite.nouns[0]} in which she lives 
-and hide in a nearby ${snowWhite.nouns[1]}.`);
+and hide in a nearby ${snowWhite.nouns[1]}.`)
 console.log('')
 console.log(`Once there, she is discovered by ${snowWhite.adjectives[1]} animals who guide her to the 
 ${snowWhite.adjectives[2]} cottage of the seven dwarfs. The dwarfs take care of her until 
 the prince, who has traveled the four corners of the ${snowWhite.nouns[2]} in search of 
 Snow ${snowWhite.color}, arrives and gives her a magical ${snowWhite.nouns[3]} on her 
 ${snowWhite.partOfBody}, which miraculously bring her back to life after she eats 
-a poisonous ${snowWhite.nouns[4]}. Snow White and the prince live ${snowWhite.adverb} ever after.`);
+a poisonous ${snowWhite.nouns[4]}. Snow White and the prince live ${snowWhite.adverb} ever after.`)
 console.log('')
-
